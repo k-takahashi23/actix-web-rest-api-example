2cd8f790281eae8607a6a75fcbf488ba9762787f
k-takahashi23 <efl.sad23@gmail.com>
2021-12-31 02:04:35 +0900

    Move tests

--- application/src/usecases/users/create_user/interactor.rs
+++ application/src/usecases/users/create_user/interactor.rs
@@ -9,3 +9,55 @@ impl<T: UsersRepository> CreateUserUsecase<T> {
         CreateUserOutput{ id: saved_user.id, name: saved_user.name, age: saved_user.age }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use domain::{repositories::users::UsersRepository, entities::user::User};
+    use crate::usecases::users::create_user::{usecase::CreateUserUsecase, input::CreateUserInput, output::CreateUserOutput};
+
+    // TODO: Mock ライブラリ使う
+    pub struct MockUsersRepository {}
+    impl UsersRepository for MockUsersRepository {
+        fn new() -> Self {
+            MockUsersRepository {}
+        }
+    
+        fn find(&self, id: String) -> User {
+            User {
+                id,
+                name: "mock_name_test".to_string(),
+                age: 30,
+            }
+        }
+    
+        fn save(&self, user: User) -> User {
+            user
+        }
+    
+        fn delete(&self, id: String) -> User {
+            User {
+                id,
+                name: "mock_name_test".to_string(),
+                age: 20,
+            }
+        }   
+    }
+
+    #[test]
+    fn create_user_ok() {
+        // Given
+        let repository = MockUsersRepository::new();
+        let usecase = CreateUserUsecase(repository);
+        let input = CreateUserInput { name: "mock_name_test".to_string(), age: 30 };
+        let expected = CreateUserOutput { id: "1".to_string(), name: "mock_name_test".to_string(), age: 30 };
+
+        // When
+        let actual = usecase.handle(input);
+
+        // Then
+        // TODO: uuid のモック化
+        // assert_eq!(expected.id, actual.id);
+        assert_eq!(expected.name, actual.name);
+        assert_eq!(expected.age, actual.age);
+    }
+}
