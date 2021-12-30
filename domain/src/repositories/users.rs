2cfe9d6be73b8c0bf2687034f252d7583c652c03
k-takahashi23 <efl.sad23@gmail.com>
2021-12-30 17:39:37 +0900

    Change tab

--- domain/src/repositories/users.rs
+++ domain/src/repositories/users.rs
@@ -1,8 +1,8 @@
 use crate::entities::user::User;
 
 pub trait UsersRepository {
-  fn new() -> Self;
-  fn find(&self, id: String) -> User;
-  fn save(&self, user: User) -> User;
-  fn delete(&self, id: String) -> User;
+    fn new() -> Self;
+    fn find(&self, id: String) -> User;
+    fn save(&self, user: User) -> User;
+    fn delete(&self, id: String) -> User;
 }
